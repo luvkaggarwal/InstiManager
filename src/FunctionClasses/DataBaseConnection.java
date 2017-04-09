a53df39cb55406791a23342bbfb8b457a7592acd
Luv Aggarwal <luvkaggarwal@gmail.com>
2017-04-09 12:47:21 +0530

    Added Drivers and starter code

---
+++ src/FunctionClasses/DataBaseConnection.java
@@ -0,0 +1,28 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package FunctionClasses;
+import java.sql.*;
+import javax.swing.*;
+/**
+ *
+ * @author LUV
+ */
+public class DataBaseConnection {
+    Connection conn = null;
+    public static Connection ConnectDB(){
+        
+        try{
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/kenbit");
+            return conn;
+        }catch( Exception e ){
+            JOptionPane.showMessageDialog( null,e );
+            return null;
+        }
+    }
+}
